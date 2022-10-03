79bcc666bb444191ddae6ae0e5804376be6bb65c
brodie02 <marshallbrodie0@gmail.com>
2022-10-03 12:21:28 +1030

    Add all time elements

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,25 @@
 var currentDayEl = $("#currentDay")
+var inputEl = $("input")
+
+var nineEl = $("#nine")
+var tenEl = $("#nine")
+var elevenEl = $("#nine")
+var twelveEl = $("#nine")
+var oneEl = $("#nine")
+var twoEl = $("#nine")
+var threeEl = $("#nine")
+var fourEl = $("#nine")
+var fiveEl = $("#nine")
+
+var nineOClock = moment("09:00", "kk:mm").format("kk:mm")
+var tenOClock = moment("10:00", "kk:mm").format("kk:mm")
+var elevenOClock = moment("11:00", "kk:mm").format("kk:mm")
+var twelveOClock = moment("12:00", "kk:mm").format("kk:mm")
+var oneOClock = moment("13:00", "kk:mm").format("kk:mm")
+var twoOClock = moment("14:00", "kk:mm").format("kk:mm")
+var threeOClock = moment("15:00", "kk:mm").format("kk:mm")
+var fourOClock = moment("16:00", "kk:mm").format("kk:mm")
+var fiveOClock = moment("17:00", "kk:mm").format("kk:mm")
 
 // Prints the current day in the header
 $(function renderCurrentDay() {
@@ -8,5 +29,11 @@ $(function renderCurrentDay() {
 
 // Changes the colour of the hour columns depending on the hour it is
 $(function hourColoumnColour() {
+    var currentTime = moment().format("kk:mm")
 
+    if (currentTime < nineOClock) {
+        inputEl.removeClass("past")
+        inputEl.addClass("future")
+    } else if (nineOClock > currentTime < tenOClock)
+        inputEl.a
 })
